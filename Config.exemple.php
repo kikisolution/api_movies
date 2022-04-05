5fa0f4b8b97532aebf72807b570a1bb1c481e25f
Ezequias Fonseca <ezequiasfonseca@gmail.com>
2022-04-05 20:20:47 -0300

    file config

--- Config.exemple.php
+++ Config.exemple.php
@@ -1,7 +1,7 @@
 <?php
 
 //Api
-define('API_URL', "http://localhost:9000/api"); 
+define('API_URL', "http://localhost:9000/api");
 define("API_KEY", "d41d8cd98f00b204e9800998ecf8427e");
 
 ##DB - CONNECTION
